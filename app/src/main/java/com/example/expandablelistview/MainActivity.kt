42dce8d1f82be64dd3ff4a0d915ae7f77ab0d2cf
MosesWangira <40572173+MosesWangira@users.noreply.github.com>
2021-01-09 09:57:33 +0300

    apply dataBinding to MainActivity

--- app/src/main/java/com/example/expandablelistview/MainActivity.kt
+++ app/src/main/java/com/example/expandablelistview/MainActivity.kt
@@ -1,54 +1,86 @@
 package com.example.expandablelistview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ExpandableListAdapter
 import android.widget.ExpandableListView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.expandablelistview.ExpandableListData.data
+import com.example.expandablelistview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var expandableListView: ExpandableListView? = null
-    private var adapter: ExpandableListAdapter? = null
-    private var titleList: List<String>? = null
+
+    lateinit var binding: ActivityMainBinding
+
+    lateinit var expandableListView: ExpandableListView
+    lateinit var adapter: ExpandableListAdapter
+    lateinit var titleList: List<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        expandableListView = findViewById(R.id.expendableList)
-        if (expandableListView != null) {
-            val listData = data
-            titleList = ArrayList(listData.keys)
-            adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
-            expandableListView!!.setAdapter(adapter)
-            expandableListView!!.setOnGroupExpandListener { groupPosition ->
-                Toast.makeText(
-                        applicationContext,
-                        (titleList as ArrayList<String>)[groupPosition] + " List Expanded.",
-                        Toast.LENGTH_SHORT
-                ).show()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        expandableListView = binding.expendableList
+
+        val listData = data
+        titleList = ArrayList(listData.keys)
+        adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+
+        expandableListView.apply {
+            adapter = adapter
+            setOnGroupExpandListener { groupPosition ->
+                toast((titleList as ArrayList<String>)[groupPosition] + " List Expanded.")
             }
-            expandableListView!!.setOnGroupCollapseListener { groupPosition ->
-                Toast.makeText(
-                        applicationContext,
-                        (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
-                        Toast.LENGTH_SHORT
-                ).show()
+            setOnGroupCollapseListener { groupPosition ->
+                toast((titleList as ArrayList<String>)[groupPosition] + " List Collapsed.")
             }
-            expandableListView!!.setOnChildClickListener { _, _, groupPosition, childPosition, _ ->
-                Toast.makeText(
-                        applicationContext,
-                        "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(
-                                titleList as
-                                        ArrayList<String>
-                                )
-                                [groupPosition]]!!.get(
-                                childPosition
-                        ),
-                        Toast.LENGTH_SHORT
-                ).show()
+            setOnChildClickListener { _, _, groupPosition, childPosition, _ ->
+                toast("Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(
+                        titleList as
+                                ArrayList<String>
+                        )
+                        [groupPosition]]!![childPosition])
                 false
             }
         }
+
+//        val listData = data
+//        titleList = ArrayList(listData.keys)
+//        adapter = CustomExpandableListAdapter(this, titleList as ArrayList<String>, listData)
+//        expandableListView.setAdapter(adapter)
+//        expandableListView.setOnGroupExpandListener { groupPosition ->
+//            Toast.makeText(
+//                applicationContext,
+//                (titleList as ArrayList<String>)[groupPosition] + " List Expanded.",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//        expandableListView.setOnGroupCollapseListener { groupPosition ->
+//            Toast.makeText(
+//                applicationContext,
+//                (titleList as ArrayList<String>)[groupPosition] + " List Collapsed.",
+//                Toast.LENGTH_SHORT
+//            ).show()
+//        }
+//        expandableListView.setOnChildClickListener { _, _, groupPosition, childPosition, _ ->
+//            Toast.makeText(
+//                applicationContext,
+//                "Clicked: " + (titleList as ArrayList<String>)[groupPosition] + " -> " + listData[(
+//                        titleList as
+//                                ArrayList<String>
+//                        )
+//                        [groupPosition]]!!.get(
+//                    childPosition
+//                ),
+//                Toast.LENGTH_SHORT
+//            ).show()
+//            false
+//        }
+    }
+
+    private fun toast(toastMessage: String){
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
     }
 }
